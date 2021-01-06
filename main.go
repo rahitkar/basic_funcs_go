918f922416ab7a5afd6d6db4ded6548c88a158d5
Rahit Kar <58026094+rahitkar@users.noreply.github.com>
2021-01-06 17:39:24 +0530

    added go.mod and go.sum optimized imports

--- main.go
+++ main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"fmt"
-	"./src/factorial"
-	"./src/series"
+	"rsc.io/quote"
+	"step.com/main/src/factorial"
+	"step.com/main/src/series"
 )
 
 func main() {
 	fmt.Println("factorial of 5 = ", factorial.Calculate(5))
 
-	fiboSeries := make([]int, 0)
-	fiboSeries = append(fiboSeries, 0)
-	fiboSeries = append(fiboSeries, 1)
-	result := series.Fibonacci(fiboSeries, 6)
-	fmt.Println(fiboSeries)
+	result := series.Fibonacci(6)
 	fmt.Println(result)
+
+	fmt.Println(quote.Go())
 }
--- src/series/series.go
+++ src/series/series.go
@@ -1,21 +1,26 @@
 package series
-// Fibonacci returns fiboSeries in from of an array upto the given limit
-// func Fibonacci(to int) []int {
-// 	fiboSeries := make([]int, 0)
-// 	fiboSeries = append(fiboSeries, 0)
-// 	fiboSeries = append(fiboSeries, 1)
-
-// 	for i := 2; i < to; i++ {
-// 		fiboSeries = append(fiboSeries, (fiboSeries[i - 1] + fiboSeries[i - 2])) 
-// 	}
-// 	return fiboSeries;
-// }
 
 // Fibonacci returns fiboSeries in from of an array upto the given limit
-func Fibonacci(series []int, to int) []int {
-	length := len(series)
-	if length >= to {
-		return series
+func Fibonacci(to int) []int {
+	fiboSeries := make([]int, 0)
+
+	pri := -1
+	next := 1
+
+	for i := 0; i < to; i++ {
+		sum := pri + next
+		fiboSeries = append(fiboSeries, sum)
+		pri = next
+		next = sum
 	}
-	return Fibonacci(append(series, series[length - 1] + series[length - 2]), to);
+	return fiboSeries
 }
+
+// Fibonacci returns fiboSeries in from of an array upto the given limit
+// func Fibonacci(series []int, to int) []int {
+// 	length := len(series)
+// 	if length >= to {
+// 		return series
+// 	}
+// 	return Fibonacci(append(series, series[length - 1] + series[length - 2]), to);
+// }
