388d5ddc9f2cf3042da1d5bc23f01a1b272c0b2a
Rahit Kar <58026094+rahitkar@users.noreply.github.com>
2021-01-07 03:20:03 +0530

    updated main

--- main.go
+++ main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"fmt"
 	"rsc.io/quote"
+	"step.com/basic_funcs/src/closures"
 	"step.com/basic_funcs/src/factorial"
 	"step.com/basic_funcs/src/series"
+	"step.com/basic_funcs/src/shape"
 )
 
 func main() {
+	nextInt := closures.Incrementer()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	square := closures.Squarer(2)
+	fmt.Println(square())
+	fmt.Println(square())
+
 	fmt.Println("factorial of 5 = ", factorial.Calculate(5))
 
 	result := series.Fibonacci(6)
 	fmt.Println(result)
+
+	rectangle := shape.Rectangle{Length: 4, Breath: 6}
+	fmt.Println(shape.CalculateArea(rectangle))
+
+	circle := shape.Circle{Radius: 2}
+	fmt.Println(shape.CalculateArea(circle))
 
 	fmt.Println(quote.Go())
 }
